34c82b41fec69468c743493d7adc76eec502f848
famousgarkin <famousgarkin@outlook.com>
2014-12-15 23:17:49 +0100

    added options

--- bootstrap-popover-mouseover.js
+++ bootstrap-popover-mouseover.js
@@ -1,31 +1,33 @@
 (function($) {
-    var hoveredFlag = 'popover-hovered'
-    var delay = 100
+    $.fn.mouseoverPopover = function(options) {
+        var options = $.extend({
+            class: 'popover-hovered',
+            delay: 100
+        }, options)
 
-    $.fn.mouseoverPopover = function() {
         this.mouseover(function(e) {
-            $(this).popover('show').addClass(hoveredFlag)
+            $(this).popover('show').addClass(options.class)
         })
         .mouseleave(function(e) {
             var item = $(this)
             setTimeout(function() {
-                if (item.hasClass(hoveredFlag)) {
+                if (item.hasClass(options.class)) {
                     item.popover('hide')
                 }
-            }, delay)
+            }, options.delay)
         })
         .on('shown.bs.popover', function(e) {
             var item = $(this)
             var popover = $(this).next()
             popover.mouseenter(function(e) {
-                item.removeClass(hoveredFlag)
+                item.removeClass(options.class)
             })
             .mouseleave(function(e) {
                 setTimeout(function() {
-                    if (!item.hasClass(hoveredFlag)) {
+                    if (!item.hasClass(options.class)) {
                         item.popover('hide')
                     }
-                }, delay)
+                }, options.delay)
             })
         })
         return this
